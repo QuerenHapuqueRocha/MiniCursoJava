a9c9382d3873565357d97573f3317fbcf51ea232
Queren Hapuque <queren.fernandes@yahoo.com>
2022-09-14 08:04:28 -0300

    Add files via upload

---
+++ funcoes/src/funcoes/funcoes.java
@@ -0,0 +1,11 @@
+package funcoes;
+
+public class funcoes {
+    public static void main(String[] args) {
+        String nomeOriginal = "Queren Hapuque";
+        saudacao(nomeOriginal);
+    }
+    public static void saudacao(String nomeParametro) {
+        System.out.println("Hello, " + nomeParametro);
+    }
+}
