5b7a1bbe4dfb450f80f343a758c2862511118c87
Queren Hapuque <queren.fernandes@yahoo.com>
2022-09-14 08:04:42 -0300

    Add files via upload

---
+++ funcao2/src/funcao2/funcao2.java
@@ -0,0 +1,11 @@
+package funcao2;
+
+public class funcao2 {
+    public static void main(String[] args) {
+        int resultado = soma(2,3);
+        System.out.println(resultado);
+    }
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
